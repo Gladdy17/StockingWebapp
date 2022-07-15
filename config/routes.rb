1fab0cf21779b3502d0f699d71cb3b0322b71c4c
Gladdy17 <gladston.bosco@gmail.com>
2022-07-14 19:23:38 -0600

    Added db migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  resources :stocks
   devise_for :users
   # get 'home/index'
   root 'home#index'
   get 'home/about'
 
   post "/" => 'home#index'
+
+  # devise_for :users
+
+devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
